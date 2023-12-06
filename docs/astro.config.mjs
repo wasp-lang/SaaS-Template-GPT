e4ecfdfb186b50e54040db65617c1d8ef2bff55e
vincanger <70215737+vincanger@users.noreply.github.com>
2023-12-06 12:44:18 +0100

    fix docs header and links

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog';
+import { BASE_URL } from './constants';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://opensaas.sh',
+  site: BASE_URL,
   integrations: [
     starlightBlog({
       title: 'Blog',
